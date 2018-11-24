213d08a3d50185a7ec1c84c40d64cc23c6395edc
Tobias Lundgren <tobias@lundgrendesign.se>
2018-11-24 14:01:12 +0100

    Add styles config

---
+++ client/src/styles.js
@@ -0,0 +1,14 @@
+const colors = {
+  brandColor: 'rgb(32, 0, 255)',
+  yellow: '#EAD50F',
+  backgroundColor: 'white',
+  fontColor: '#222',
+  gray: '#aaaaaa',
+};
+
+const styles = {
+  colors,
+  boxShadow: '0 4px 12px 0 rgba(0, 0, 0, .06)',
+};
+
+export default styles;
