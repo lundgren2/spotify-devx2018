1e5195d3f6d56c910d4c51df5781212c78446ccd
Tobias Lundgren <tobias@lundgrendesign.se>
2018-11-24 21:05:41 +0100

    Break out client variables

---
+++ client/src/config.example.js
@@ -0,0 +1,8 @@
+const config = {
+  BASE: 'https://accounts.spotify.com/authorize',
+  CLIENT_ID: '', // add your client_id here
+  RESPONSIVE_TYPE: 'code',
+  REDIRECT_URI: 'http://localhost:3000/dashboard',
+};
+
+export default config;
