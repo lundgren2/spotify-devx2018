6278f2e2c66c83802024e1afe10cceb6bf91a489
KardaChalak <karda.chalak@gmail.com>
2018-11-25 12:14:19 +0100

    Remove mock data

--- client/src/components/Overview/index.js
+++ client/src/components/Overview/index.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import Container from '../ui/Container';
-import fakePlaylist from './fakePlaylist';
 import Emoji from '../Emoji';
 import Button from '../ui/Button';
 import { getPlaylistAudioInfo } from '../../utils/service';
