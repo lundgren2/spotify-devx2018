a12110c48371f014db4d1c6abe0ee3f9fba6c113
Tobias Lundgren <tobias@lundgrendesign.se>
2018-11-25 11:04:01 +0100

    MyEmotion done

--- client/src/components/MyEmotion.js
+++ client/src/components/MyEmotion.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import Emoji from './Emoji';
+import { Link } from 'react-router-dom';
 
-const MyEmotion = ({ emoji, color }) => {
+const MyEmotion = props => {
+  const { emoji, color, text } = props.location.state;
+
   const style = {
     container: {
       width: '100vw',
@@ -9,16 +12,24 @@ const MyEmotion = ({ emoji, color }) => {
       position: 'relative',
       background: color,
       display: 'flex',
+      flexDirection: 'column',
       justifyContent: 'center',
       alignItems: 'center',
       verticalAlign: 'middle',
       fontSize: '670%',
     },
   };
+
   return (
-    <div style={style.container}>
-      <Emoji symbol={emoji} />
-    </div>
+    <Link to="/overview">
+      <div style={style.container}>
+        <div>
+          <Emoji symbol={emoji} style={{ display: 'block' }} />
+          <br />
+          <h1 style={{ fontSize: 84, color: 'white' }}>{text}</h1>
+        </div>
+      </div>
+    </Link>
   );
 };
 
