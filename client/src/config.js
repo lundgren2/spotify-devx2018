ed8d17fcea8d187e7c935652ad0ab8890f554a56
Tobias Lundgren <tobias@lundgrendesign.se>
2018-11-25 11:34:08 +0100

    remove config

--- client/src/config.js
+++
@@ -1,8 +0,0 @@
-const config = {
-  BASE: 'https://accounts.spotify.com/authorize',
-  CLIENT_ID: '167d9bf647524ef28ea579584c33e6bb',
-  RESPONSIVE_TYPE: 'code',
-  REDIRECT_URI: 'http://localhost:3000/dashboard',
-};
-
-export default config;
