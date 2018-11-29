921aa6bf93bd7eb71cdb2b4e47d0ae06f13d2a98
Tobias Lundgren <tobias@lundgren.design>
2018-11-29 22:57:15 +0100

    Add redirect uri for production

--- src/config.js
+++ src/config.js
@@ -1,3 +1,7 @@
+const REDIRECT_URI = window.location.href.includes('localhost')
+  ? 'http://localhost:3000'
+  : 'https://socify-app.herokuapp.com';
+
 const BACKEND_URI = window.location.href.includes('localhost')
   ? 'http://localhost:8888'
   : 'https://socify-server.herokuapp.com';
@@ -5,7 +9,7 @@ const BACKEND_URI = window.location.href.includes('localhost')
 const config = {
   CLIENT_ID: '57553618157440e19f8f0a747dde4acd',
   SPOTIFY_API: 'https://api.spotify.com/v1',
-  REDIRECT_URI: 'http://localhost:3000 ',
+  REDIRECT_URI,
   BACKEND_URI,
 };
 
