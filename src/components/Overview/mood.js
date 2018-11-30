da1dde662a7cda9f0dc4eec4164688589be12f5d
Tobias Lundgren <tobias@lundgren.design>
2018-11-30 14:00:11 +0100

    Update emojis

--- src/components/Overview/mood.js
+++ src/components/Overview/mood.js
@@ -1,16 +1,4 @@
-export const MOOD_HAPPY = {
-  EMOJI: '😎🤗',
-  COLOR: 'rgba(185, 36, 59, 1)',
-};
-export const MOOD_SAD = {
-  EMOJI: '😕😢',
-  COLOR: 'rgba(123, 31, 162, 1)',
-};
-export const MOOD_ANGRY = {
-  EMOJI: '😤😠',
-  COLOR: 'rgba(39, 38, 38, 1)',
-};
-export const MOOD_PARTY = {
-  EMOJI: '🥳🕺',
-  COLOR: 'rgba(220, 112, 43, 1)',
-};
+export const MOOD_HAPPY = { EMOJI: '😎🤗', COLOR: 'rgba(185, 36, 59, 1)' };
+export const MOOD_SAD = { EMOJI: '🙁😢', COLOR: 'rgba(123, 31, 162, 1)' };
+export const MOOD_ANGRY = { EMOJI: '😤😠', COLOR: 'rgba(39, 38, 38, 1)' };
+export const MOOD_PARTY = { EMOJI: '🥳🕺', COLOR: 'rgba(220, 112, 43, 1)' };
